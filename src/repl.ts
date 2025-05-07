3ce027edc3a1ba713d4bf2038dd60ce116c94e84
bzelaznicki <bartosz@zelaznicki.com>
2025-05-07 12:21:13 +0200

    starting an actual repl

--- src/repl.ts
+++ src/repl.ts
@@ -1,4 +1,26 @@
+import * as readline from 'node:readline';
 export function cleanInput(input: string): string[] {
-    const result = input.trim().toLowerCase().split(/\s+/);
+    const result = input.trim().toLowerCase().split(/\s+/).filter(word => word !== '');
     return result;
+  }
+
+export function startREPL() {
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+});
+    rl.prompt();
+    rl.on('line', (line: string) => {
+        const cleanedInput = cleanInput(line);
+
+        if (cleanedInput.length === 0){
+            rl.prompt();
+            return;
+        }
+        console.log(`Your command was: ${cleanedInput[0]}`);
+        rl.prompt();
+    })
+
 }
